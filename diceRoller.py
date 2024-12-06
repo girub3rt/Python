8e3961107d4b6f61812b80b4acb67a4dca485c5d
Emma Alhola <esalho@utu.fi>
2024-12-06 07:53:00 +0200

    Testing git

---
+++ diceRoller.py
@@ -0,0 +1,28 @@
+import random
+import time
+
+rolls = 0
+
+while True:
+    choice = input("Roll the dice? (y/n): ").lower()
+    if choice == "n":
+        if rolls == 0:
+            print("You didn't even try! :/")
+            break
+        else:
+            print(f"You rolled {rolls} times.")
+            print("Thank you for playing!")
+            break
+
+    if choice == "y":
+        try:
+            many = int(input("How many times do you roll? "))
+            for i in range(many):
+                print(f"Roll {i + 1}: {random.randint(1, 6)}")
+            rolls += 1
+        except ValueError:
+            print("Please enter a number.")
+    else:
+        print("Your dice fell to the floor. :(")
+        print("Try again!")
+        time.sleep(2)
